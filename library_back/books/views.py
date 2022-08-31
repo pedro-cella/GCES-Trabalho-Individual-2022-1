9f525a93d816edb5ae4a0be77e750d130087de49
Pedro Cella <pedrovscella@gmail.com>
2022-08-31 10:32:51 -0300

    Testins flake8 ignore

--- library_back/books/views.py
+++ library_back/books/views.py
@@ -1,19 +1,20 @@
+# flake8: noqa
 from rest_framework.viewsets import ModelViewSet
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.decorators import action
-# from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated 
 from rest_framework.response import Response
 from .models import Book
 from .serializers import BookSerializer
 
-
 class BookViewSet(ModelViewSet):
     serializer_class = BookSerializer
     # permission_classes = [IsAuthenticated]
+
     def get_queryset(self):
-        renter = self.request.query_params.get('renter', None) 
-        is_rented = self.request.query_params.get('is_rented', None)
+        renter = self.request.query_params.get('renter', None)       
+        is_rented = self.request.query_params.get('is_rented', None) 
 
         if renter:
             return Book.objects.filter(renter=renter)
@@ -27,7 +28,7 @@ class BookViewSet(ModelViewSet):
     def rent(self, request, pk=None):
         book = self.get_object()
         
-        if 'renter' not in request.data.keys():
+        if not 'renter' in request.data.keys():
             return Response(
                 {'error': 'could not retrieve renter from request'},
                 status=status.HTTP_400_BAD_REQUEST
@@ -35,7 +36,7 @@ class BookViewSet(ModelViewSet):
 
         renter_id = request.data['renter'] or None
         renter = User.objects.filter(id=renter_id).first()
-
+            
         if book.is_rented:
             book.renter = None
         else:
